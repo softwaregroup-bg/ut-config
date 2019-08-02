f6ffd4c6f5dbbec3eff7b051916c4403e7f80fae
stambata <stamen.peev@softwaregroup-bg.com>
2019-08-02 03:50:18 +0300

    feat: advanced merge (#1)

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const yaml = require('yaml');
 const ini = require('ini');
 const stripJsonComments = require('strip-json-comments');
 const rc = require('rc');
-const merge = require('ut-port/merge');
+const merge = require('ut-merge');
 const path = require('path');
 const serverRequire = require;
 const fs = require('fs');
-const mergeWith = require('lodash.mergewith');
 
 function parse(content) {
     if (/^\s*{/.test(content)) return JSON.parse(stripJsonComments(content));
@@ -20,22 +19,6 @@ function parse(content) {
     return ini.parse(content);
 }
 
-const convert = value => mergeWith({}, value, (_, src) => {
-    if (typeof src === 'string') {
-        switch (src) {
-            case 'true':
-                return true;
-            case 'false':
-                return false;
-            case 'null':
-                return null;
-            default:
-                const float = parseFloat(src);
-                if (!isNaN(float) && (float + '') === src) return float;
-        }
-    }
-});
-
 const edit = ({edit, formData, filename, log, stop = true, validate = false}) => {
     if (edit) {
         if (validate && edit.schema) {
@@ -45,7 +28,7 @@ const edit = ({edit, formData, filename, log, stop = true, validate = false}) =>
         if (!validate) {
             if (filename) {
                 try {
-                    formData = convert(parse(fs.readFileSync(filename, 'utf-8')));
+                    formData = merge([{}, parse(fs.readFileSync(filename, 'utf-8'))], {convert: true});
                 } catch (e) {}
             }
             return require('ut-form-jsonschema').edit({
@@ -68,21 +51,27 @@ function mount(parent, m) {
     }
 }
 
-function load(params = {}) {
-    var argv = require('minimist')(process.argv.slice(2));
-    let config = params.config;
-    if (Array.isArray(config)) config = merge({}, ...config);
-    if (!config) {
-        config = {params: {}, version: params.version};
-        config.params.app = process.env.UT_APP || params.app || argv._[0] || 'server';
-        config.params.method = process.env.UT_METHOD || params.method || argv._[1] || 'debug';
-        config.params.env = process.env.UT_ENV || params.env || argv._[2] || 'dev';
-        config.service = config.params.app + '/' + config.params.env;
-        const appPath = path.dirname(params.resolve('./' + config.params.app));
-        mount(params.root, config.params.app);
+function load({ params, app, method, env, root, version, resolve, config } = {}) {
+    const argv = require('minimist')(process.argv.slice(2));
+    const baseConfig = {
+        version,
+        params: {
+            app: process.env.UT_APP || app || argv._[0] || 'server',
+            method: process.env.UT_METHOD || method || argv._[1] || 'debug',
+            env: process.env.UT_ENV || env || argv._[2] || 'dev'
+        }
+    };
+    baseConfig.service = baseConfig.params.app + '/' + baseConfig.params.env;
+    const configs = [];
+    if (config) {
+        if (Array.isArray(config)) configs.push(...config);
+        else configs.push(config);
+    } else {
+        const appPath = path.dirname(resolve('./' + baseConfig.params.app));
+        mount(root, baseConfig.params.app);
         // load and merge configurations
-        const configFilenames = ['common', config.params.method, config.params.env];
-        const configs = configFilenames
+        const configFilenames = ['common', baseConfig.params.method, baseConfig.params.env];
+        const implConfigs = configFilenames
             .map(filename => {
                 let configPath;
                 try {
@@ -91,33 +80,35 @@ function load(params = {}) {
                 return configPath && require(configPath);
             })
             .filter(x => x);
-        if (!configs.length) {
+        if (!implConfigs.length) {
             throw new Error(`${configFilenames.join(' and/or ')} configuration must be provided`);
         }
-        merge(config, ...configs, params.params);
-    } else {
-        if (!config.params) {
-            config.params = {};
-        }
-        if (!config.params.app) {
-            config.params.app = process.env.UT_APP || params.app || argv._[0] || 'server';
-        }
-        if (!config.params.method) {
-            config.params.method = process.env.UT_METHOD || params.method || argv._[1] || 'debug';
-        }
-        if (!config.params.env) {
-            config.params.env = process.env.UT_ENV || params.env || argv._[2] || 'dev';
-        }
-        config.service = config.params.app + '/' + config.params.env;
+        configs.push(...implConfigs);
     }
 
-    config.params.appname = config.params.appname || [
-        'ut',
-        (config.implementation || 'ut5').replace(/[-/\\]/g, '_'),
-        process.env.UT_ENV || params.env || 'dev'
-    ].join('_');
+    if (params) configs.push(params);
 
-    return convert(rc(config.params.appname, config, argv, parse));
+    let {
+        appname,
+        mergeOptions = {},
+        implementation = 'ut5'
+    } = merge({}, ...configs.map((config = {}) => {
+        return {
+            appname: config.params && config.params.appname,
+            mergeOptions: config.merge,
+            implementation: config.implementation
+        };
+    }));
+
+    if (!appname) {
+        baseConfig.params.appname = appname = `ut_${implementation.replace(/[-/\\]/g, '_')}_${baseConfig.params.env}`;
+    }
+
+    configs.push(rc(appname, {}, argv, parse));
+
+    configs.unshift(baseConfig);
+
+    return merge(configs, {...mergeOptions, convert: true});
 }
 
 module.exports = {load, edit, merge};
