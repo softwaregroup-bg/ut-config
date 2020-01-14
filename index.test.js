02ca34fdb6c8d597814660e536fc1f37dfb86003
stamen.peev <stamen.peev@softwaregroup-bg.com>
2020-01-14 16:27:53 +0200

    test: add unit tests

---
+++ index.test.js
@@ -0,0 +1,51 @@
+/*  eslint no-process-env: 0, no-template-curly-in-string: 0 */
+const tap = require('tap');
+const { load } = require('.');
+const sortKeys = require('sort-keys');
+const crypto = require('crypto');
+
+const algorithm = process.env.UT_DECRYPT_ALGORITHM = 'aes-256-cbc';
+const key = process.env.UT_DECRYPT_KEY = '12345678901234567890123456789012';
+const iv = process.env.UT_DECRYPT_IV = '1234567890123456';
+
+const cipher = crypto.createCipheriv(algorithm, key, iv);
+
+let encrypted = cipher.update('test', 'utf8', 'hex');
+encrypted += cipher.final('hex');
+
+tap.test('load', assert => {
+    assert.matchSnapshot(sortKeys(load({
+        config: {
+            a: [`\${decrypt('${encrypted}')}`, 'ordinary string'],
+            b: `\${decrypt('${encrypted}')}`,
+            c: {
+                d: `\${decrypt('${encrypted}')}`,
+                e: ['ordinary string'],
+                f: 'ordinary string',
+                g: {
+                    h: 'ordinary string'
+                }
+            }
+        }
+    }), {deep: true}), 'decrypt');
+
+    assert.matchSnapshot(sortKeys(load({
+        config: {
+            a: [`\${decrypt('${encrypted}')}`, '${custom("ordinary string")}'],
+            b: `\${custom(decrypt('${encrypted}'))}`,
+            c: {
+                d: `\${decrypt('${encrypted}')}`,
+                e: ['ordinary string'],
+                f: 'ordinary string',
+                g: {
+                    h: '${custom("ordinary string")}'
+                }
+            }
+        },
+        context: {
+            custom: str => str + ' - custom'
+        }
+    }), {deep: true}), 'custom context');
+
+    assert.end();
+});
---
+++ tap-snapshots/index.test.js-TAP.test.js
@@ -0,0 +1,64 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`index.test.js TAP load > custom context 1`] = `
+Object {
+  "_": Array [],
+  "a": Array [
+    "test",
+    "ordinary string - custom",
+  ],
+  "b": "test - custom",
+  "c": Object {
+    "d": "test",
+    "e": Array [
+      "ordinary string",
+    ],
+    "f": "ordinary string",
+    "g": Object {
+      "h": "ordinary string - custom",
+    },
+  },
+  "params": Object {
+    "app": "server",
+    "appname": "ut_ut5_dev",
+    "env": "dev",
+    "method": "debug",
+  },
+  "service": "server",
+  "version": undefined,
+}
+`
+
+exports[`index.test.js TAP load > decrypt 1`] = `
+Object {
+  "_": Array [],
+  "a": Array [
+    "test",
+    "ordinary string",
+  ],
+  "b": "test",
+  "c": Object {
+    "d": "test",
+    "e": Array [
+      "ordinary string",
+    ],
+    "f": "ordinary string",
+    "g": Object {
+      "h": "ordinary string",
+    },
+  },
+  "params": Object {
+    "app": "server",
+    "appname": "ut_ut5_dev",
+    "env": "dev",
+    "method": "debug",
+  },
+  "service": "server",
+  "version": undefined,
+}
+`
