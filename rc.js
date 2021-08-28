eddfd82fa88f5f3734a263f6206b2cfbbb57ee20
Kalin Krustev <kalin.krustev@softwaregroup.com>
2021-08-28 11:19:58 +0300

    feat: load configuration from the browser

--- rc.js
+++ rc.js
@@ -1,1 +1,3 @@
-module.exports = (appname, conf) => conf;
+const merge = require('ut-function.merge');
+module.exports = (appname, conf) =>
+    window.localStorage ? merge({}, conf, JSON.parse(window.localStorage.getItem(`.${appname}rc`))) : conf;
